26b1b9c6b0f2199a327ec6ccab2dd4a03537c78f
Sourabh Singhal <saurabhsinghal3536@gmail.com>
2021-12-05 01:11:55 +0530

    First Commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,22 @@
+const Sdata = [
+    {
+        sname:"DARK",
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title:"A Netflix Original Series",
+        links: ""
+    },
+    {
+        sname:"Stranger Things",
+        imgsrc:"https://wallpapercave.com/wp/wp1917154.jpg",
+        title:"A Netflix Original Series",
+        links: ""
+    },
+    {
+        sname:"The Vampire Diaries",
+        imgsrc:"https://images.justwatch.com/poster/8589251/s592",
+        title:"An Amazon Original Series",
+        links: ""
+    },
+]
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from './Card';
+import Sdata from './Sdata';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+  <h1 className="heading_style">List of top 5 Netflix series in 2020</h1>
+  {Sdata.map((val,index) => {
+  return (
+    <Card imgsrc={val.imgsrc} 
+    title={val.title} 
+    sname={val.sname} 
+    link={val.links} />
+  );
+})}
+  </>
+  ,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
